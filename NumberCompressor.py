48042a65b91fbac52e28fef70ce3fdc1f34a6645
Gabriel Luiz <96993016+gabrielluizone@users.noreply.github.com>
2022-09-02 20:23:16 -0300

    Add files via upload

---
+++ NumberCompressor.py
@@ -0,0 +1,17 @@
+df = [int(n) for n in input('Data Compression\n>> ').split(' ')]
+df.sort()
+uni = list(set(df))
+uni.sort()
+dic = dict(zip( uni , [df.count(n) for n in uni] ))
+out = []
+for i in dic:
+  if dic[i] == 1:
+    out.append(i)
+  elif dic[i] == 2:
+    out.extend([i, i])
+  elif dic[i] == 3:
+    out.extend([i, i, i])
+  else:
+    out.extend(['AA', i, dic[i]])
+prt = str(out)[1:-1].replace(',', '')
+print(f'\nFull Compression\n>> {prt}')
